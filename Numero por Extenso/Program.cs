bb221eb3b8e17cb764f188277ede1aa5e615f6f3
MariaEduardadr <mariaeduarda.reis1404@gmail.com>
2024-07-25 11:52:55 -0300

    Adicionar arquivos de projeto.

---
+++ Numero por Extenso/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_por_Extenso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número:");
+            numero = int.Parse(Console.ReadLine());
+            if (numero == 0)
+            {
+                Console.WriteLine("ZERO");
+            }
+            else if (numero == 1)
+            {
+                Console.WriteLine("UM");
+            }
+            else if (numero == 2)
+            {
+                Console.WriteLine("DOIS");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("TRÊS");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("QUATRO");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("CINCO");
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*Escreva um programa que leia um número entre 0 e 5 e imprima o valor lido por extenso (ex: “Cinco”),
+se o número estiver fora dessa faixa imprima “Valor inválido”*/
---
+++ Requisitos para carteira de habilitação/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requisitos_para_carteira_de_habilitação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
